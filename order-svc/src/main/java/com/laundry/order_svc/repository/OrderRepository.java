c7eaf83587eaaae110d512cc906fc6d2b7c7c579
trieule <trieuleb4@gmail.com>
2025-02-17 12:22:40 +0700

    Refactor: Rename order-svc to order and remove old files

--- order-svc/src/main/java/com/laundry/order_svc/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laundry.order_svc.repository;
-
-import com.laundry.order_svc.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-}
--- order-svc/src/main/resources/db/migration/V5__add_index_in_users.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX idx_users_gender_name ON users(gender,name);
-CREATE INDEX idx_users_dob ON users(dob);
